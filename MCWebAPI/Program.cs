4fefb77a7be9ef5941d8d0950c3cb4297e8ead90
fmi <fmi@FMI-146-18>
2024-04-30 09:46:13 +0300

    OpenAPI doc comments

--- MCWebAPI/Program.cs
+++ MCWebAPI/Program.cs
@@ -2,6 +2,7 @@ using MCApplicationServices.Implementations;
 using MCApplicationServices.Interfaces;
 using MCData.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,18 @@ builder.Services.AddScoped<IMovieManagementService, MovieManagementService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Movie Catalog",
+        Description = "RESTful API for project Movie Catalog"
+
+    });
+});
 
 var app = builder.Build();
 

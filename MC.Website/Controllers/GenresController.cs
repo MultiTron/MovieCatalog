42cc2b994adf59a8bfcd45e6aebef7dc5d4b1eab
fmi <fmi@FMI-146-18>
2024-05-21 11:13:26 +0300

    WCF Soap API

---
+++ MC.Website/Controllers/GenresController.cs
@@ -0,0 +1,17 @@
+﻿using MCWebWCF;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MC.Website.Controllers
+{
+    public class GenresController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using (GenresServiceClient client = new GenresServiceClient())
+            {
+                var result = await client.GetAllGenresAsync();
+                return View(result.Genres);
+            }
+        }
+    }
+}
